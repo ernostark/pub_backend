48ccda270ab0bcbf1fb0de076936d3c5b52e97f2
BjRkaZs <bajuszrekazsofi@gmail.com>
2024-11-28 19:35:23 +0100

    Backend-01

---
+++ app/Http/Controllers/api/DrinkController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Drink;
+
+class DrinkController extends Controller
+{
+    public function getDrinks() {
+
+        $drinks = Drink::all();
+        return $drinks;
+
+    }
+
+    public function getDrink( Request $request ) {
+
+        $drink = Drink::where( "drink", $request["drink"] )->first();
+        return $drink;
+
+    }
+
+    public function newDrink( Request $request ) {
+
+        $drink = new Drink();
+        $drink->drink = $request["drink"];
+        $drink->amount = $request["amount"];
+        $drink->type_id = $request["type_id"];
+        $drink->package_id = $request["package_id"];
+
+        $drink->save();
+
+        return $drink;
+
+    }
+
+    public function updateDrink( Request $request ) {
+
+        $drink = $this->getDrink( $request );
+
+        $drink->drink = $request["drink"];
+        $drink->amount = $request["amount"];
+        $drink->type_id = $request["type_id"];
+        $drink->package_id = $request["package_id"];
+
+        $drink->update();
+
+        return $drink;
+
+    }
+
+    public function destroyDrink( Request $request ) {
+
+        $drink = $this->getDrink( $request );
+
+        $drink->delete();
+
+        return $drink;
+
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\HTTP\Controllers\api\DrinkController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get( "/drinks", [DrinkController::class, "getDrinks"] );
+Route::get( "/drink", [DrinkController::class, "getDrink"] );
+Route::post( "/newdrink", [DrinkController::class, "newDrink"] );
+Route::put( "/updatedrink", [DrinkController::class, "updateDrink"] );
+Route::delete( "/destroydrink", [DrinkController::class, "destroyDrink"] );
